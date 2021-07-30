a6d9a9cba4c824797c201a551f38b052268ea2ce
Ilán Vivanco <ilanvivanco@gmail.com>
2021-07-31 01:06:20 +0200

    ✏ Add basic deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,3 +9,51 @@ SSH_PATH="$HOME/.ssh"
 KNOWN_HOSTS_PATH="$SSH_PATH/known_hosts"
 WPE_SSH_KEY_PATH="$SSH_PATH/wpe_deploy"
 
+# Setup our SSH connection & use keys.
+[ ! -d "$SSH_PATH" ] && mkdir "$SSH_PATH"
+
+# Copy secret keys to container.
+echo "$WPE_SSH_KEY" > "$WPE_SSH_KEY_PATH"
+
+if [[ $GITHUB_REF =~ ${PRD_BRANCH}$ ]]; then
+	export WPE_ENV_NAME=$PRD_ENV
+elif [[ $GITHUB_REF =~ ${STG_BRANCH}$ ]]; then
+	export WPE_ENV_NAME=$STG_ENV
+elif [[ $GITHUB_REF =~ ${DEV_BRANCH}$ ]]; then
+	export WPE_ENV_NAME=$DEV_ENV
+else
+	echo "FAILURE: Branch name is required." && exit 1
+fi
+
+# Deploy directories.
+if [ -n "$PUBLISH_PATH" ]; then
+	DIR_PATH="$PUBLISH_PATH"
+else
+	DIR_PATH=""
+fi
+
+if [ -n "$SOURCE_PATH" ]; then
+	SRC_PATH="$SOURCE_PATH"
+else
+	SRC_PATH="."
+fi
+
+# Deploy vars.
+WPE_SSH_HOST="$WPE_ENV_NAME.ssh.wpengine.net"
+WPE_SSH_USER="$WPE_ENV_NAME"@"$WPE_SSH_HOST"
+WPE_DESTINATION="$WPE_SSH_USER":sites/"$WPE_ENV_NAME"/"$DIR_PATH"
+
+# Setup our SSH Connection & use keys.
+ssh-keyscan -t rsa "$WPE_SSH_HOST" >> "$KNOWN_HOSTS_PATH"
+
+# Set key permissions.
+chmod 700 "$SSH_PATH"
+chmod 644 "$KNOWN_HOSTS_PATH"
+chmod 600 "$WPE_SSH_KEY_PATH"
+
+# Deploy via SSH.
+rsync -chav --inplace \
+	--rsh="ssh -v -p 22 -i ${WPE_SSH_KEY_PATH} -o StrictHostKeyChecking=no" \
+	$SRC_PATH "$WPE_DESTINATION"
+
+echo "SUCCESS: Site has been deployed!"
